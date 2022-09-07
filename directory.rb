95c8f4c2ea8c0dddf14f929a9bc68a36b8fa5d89
Narae Kim <narae1096@gmail.com>
2022-09-07 08:53:35 +0000

    12 only print the list if there is at least one student in there

--- directory.rb
+++ directory.rb
@@ -1,4 +1,4 @@
-@cohorts = cohorts = ["january", "feburary", "march", "april", "may", "june", "july", "august", "september",
+@cohorts = ["january", "feburary", "march", "april", "may", "june", "july", "august", "september",
   "october", "november", "december"]
 
 def input_students
@@ -6,9 +6,9 @@ def input_students
   puts "To finish, just hit return instead of name"
   #create an empty array
   students = []
-  name = gets.strip.capitalize
   #while the name is entered, repeat this code
-  while !name.empty? do
+  while name = gets.strip.capitalize do
+    break if name == ""
     puts "Please enter the contry where the student is from"
     country = gets.chomp.capitalize
     puts "Please enter the cohort which the student is in"
@@ -25,7 +25,6 @@ def input_students
     puts "Now we have #{students.count} student"
     end
     puts "Please enter the name of the student"
-    name = gets.strip.capitalize
   end
   #return the array of students
   students
@@ -33,64 +32,68 @@ end
 
 def print_header
   puts "The students of Villains Academy"
-  puts "------------".center(32)                  
+  puts "------------".center(32)  
 end
 
 def print(students)
-  i = 0
-  while i < students.count
+  if students.size > 1  
     #set count as index of students array
-    puts "#{i+1}. #{students[i][:name]} from #{students[i][:country]} (#{students[i][:cohort]} cohort)"
-    i += 1
-  end
-end
-
-def letters(students)
-  #get the lette which the students name begins with
-  puts "Please enter the first letter of name you want to find"
-  letter = gets.chomp.upcase
-  students.each do |student| 
-    #only print out if the name begins with the letter
-    if student[:name].start_with?(letter)
-      puts "#{student[:name]} from #{students[:country]} (#{student[:cohort]} cohort)"
+    students.each_with_index do |student, i|
+      puts "#{i+1}. #{students[i][:name]} from #{students[i][:country]} (#{students[i][:cohort]} cohort)"
     end
   end
 end
 
+def letters(students)
+  if students.size > 1
+  #get the lette which the students name begins with
+    puts "Please enter the first letter of name you want to find"
+    letter = gets.chomp.upcase
+    students.each do |student| 
+    #only print out if the name begins with the letter
+      if student[:name].start_with?(letter)
+        puts "#{student[:name]} from #{student[:country]} (#{student[:cohort]} cohort)"
+      end
+    end 
+  end
+end
+
 def name_by_length(students)
-  puts "The students whose name is shorter than 12: "
-  students.each do |student|
-    if student[:name].gsub(/\s+/,"").length < 12
-      puts "#{student[:name]} from #{students[:country]} (#{student[:cohort]} cohort)"
+  if students.size > 1
+    puts "The students whose name is shorter than 12: "
+    students.each do |student|
+      if student[:name].gsub(/\s+/,"").length < 12
+        puts "#{student[:name]} from #{student[:country]} (#{student[:cohort]} cohort)"
+      end
     end
   end
 end
 
 def print_by_cohort(students)
-  puts
+  if students.size > 1
   puts "Which cohort you want to meet"
-  cohort = gets.chomp
-  count = 0
-  students.map do |student|
-    if student[:cohort] == cohort
-      puts student[:name]
-      if count > 1
-      puts "We have #{count} students in #{cohort} cohort"
-      else
-      puts "We have #{count} student in #{cohort} cohort" 
+    cohort = gets.chomp
+    students.map do |student|
+      if student[:cohort] == cohort
+        puts student[:name]
       end
     end
-    count += 1
-  end  
+  end
 end
     
 def print_footer(names)
+  if names.count > 1 
   puts "Overall, we have #{names.count} great students".center(38)
+  elsif names.count == 1
+  puts "Overall, we have #{names.count} great student".center(38)
+  end
 end
 
 #nothing happens until we call the methods
 students = input_students
 print_header
 print(students)
+letters(students)
+name_by_length(students)
 print_by_cohort(students)
 print_footer(students)
